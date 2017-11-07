ba500501a1d92d4bd0d9df15726be9c6cf905533
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2017-11-06 22:19:33 -0800

    building a play game function with intro

--- Python3/jami_bagels.py
+++ Python3/jami_bagels.py
@@ -1,5 +1,23 @@
 import random
 
+def play_game():
+	print("\nWelcome to Bagels")
+	print("I will pick a 3 digit number with 3 unique digits.")
+	print("Guess a 3 digit number and I'll tell you if any of the digits are correct.\n")
+	print("I'll say Bagels or B, if none of the digits you guess are in my number.\n")
+	print("If a digit is in my number, but in the wrong place, I'll say Pico or P.\n")
+	print("If a digit is in the same place as my number, I'll say Fermi or F.\n")
+	print("Guess a number to begin")
+	mystery_num = pick_num()
+	correct = False
+	while not correct:
+		#Ask player to guess
+		#print result
+		#if FFF set correct to True
+		correct = True
+	
+
+
 def pick_num():
 	num = []
 	for int in range(0,10):
@@ -13,4 +31,7 @@ def pick_num():
 	
 	return num[0:3]
 	
-print(pick_num())
+#print(pick_num())
+
+play_game()
+
