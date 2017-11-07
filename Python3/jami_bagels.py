62e01deaffc5f2608026e6d788893e9c1f94f03a
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2017-11-06 22:00:48 -0800

    switch select a number into a function

--- Python3/jami_bagels.py
+++ Python3/jami_bagels.py
@@ -1,11 +1,16 @@
 import random
 
-num = []
-for int in range(0,10):
-	num.append(int)
+def pick_num():
+	num = []
+	for int in range(0,10):
+		num.append(int)
+		
+	#print(num)
 	
-print(num)
-
-random.shuffle(num)
-
-print(num)
+	random.shuffle(num)
+	
+	#print(num)
+	
+	return num[0:3]
+	
+print(pick_num())
