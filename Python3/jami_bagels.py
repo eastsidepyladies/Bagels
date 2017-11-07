e9d88ea8b23850949e623c14a1e4341d5e48b04b
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2017-11-06 23:00:56 -0800

    logic of giving processing player's guess

--- Python3/jami_bagels.py
+++ Python3/jami_bagels.py
@@ -9,23 +9,34 @@ def play_game():
 	print("If a digit is in the same place as my number, I'll say Fermi or F.\n")
 	print("Guess a number to begin")
 	mystery_num = pick_num()
-	correct = False
-	guess = 0
+	print(mystery_num)
+	correct = False	
 	while not correct:
 		#Ask player to guess
+		guess = input('Select a number >')
+		guess = int(guess)
 		while guess < 100 or guess > 999:
 			guess = input('Select a 3 digit number >')
 			guess = int(guess)
 		#print result
-		#if FFF set correct to True
-		correct = True
+		result = player_guess(guess, mystery_num)
+		if result == "FFF":
+			#if FFF set correct to True
+			correct = True
+			print('Fermi, Fermi, Fermi!')
+			print('Congratulations!, you guessed the correct number of ' + str(guess))
+		elif result == "B":
+			print('Bagels, Sadly none of those digits are in my number')
+		else:
+			print(result+ '. So close, you should try again.')
+		
 	
 
 
 def pick_num():
 	num = []
 	for int in range(0,10):
-		num.append(int)
+		num.append(str(int))
 		
 	#print(num)
 	
@@ -36,7 +47,26 @@ def pick_num():
 	return num[0:3]
 	
 def player_guess(guess, num):
-	pass
+	guess = str(guess)
+	pico = 0
+	fermi = 0
+	for index in range(0, 3):
+		if guess[index] == num[index]:
+			fermi += 1
+		elif guess[index] in num:
+			pico += 1
+
+	if pico < 1 and fermi < 1:
+		return 'B'
+	else:
+		result = ""
+		for f in range(0, fermi):
+			result += 'F'
+		for p in range (0, pico):
+			result += 'P'
+		return result
+		
+		
 	
 	
 #print(pick_num())
