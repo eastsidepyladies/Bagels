56fd0c4664e8165685f1b895b7baf4c4a424cb34
RimikaM <rimikamaju@gmail.com>
2017-11-06 22:32:18 -0800

    almost done implementing game

--- Python3/rimika_bagels.py
+++ Python3/rimika_bagels.py
@@ -9,6 +9,32 @@ Created on Mon Nov  6 19:09:49 2017
 # importing the libraries
 import random
 
+def another_game():
+    play = input("Do you want to play Bagels again? (yes or no):\n")
+    if play.lower() == "yes":
+        return True
+    else:
+        return False
+        
+
+def compare_numbers(guess, number):
+    clue = []
+    length = len(guess)
+    if guess == number:
+        return ("You win! Congrats!")
+    
+    for i in range(length):
+        if guess[i] == number[i]:
+            clue.append("Fermi")
+        elif guess[i] in number:
+            clue.append("Pico")
+    if len(clue) == 0:
+        return ("Bagels")
+    
+    clue.sort()
+    clue_str = ' '.join(clue)
+    return clue_str
+
 def valid_number(guess):
     nums = '0 1 2 3 4 5 6 7 8 9'.split()
     if guess == "":
@@ -34,10 +60,16 @@ def main():
     
     number = get_random_number()
     print("I have thought up a number. You have 10 guesses to get it")
-    for i in range(0,13):
-        guess = input("Guess #" + i + ": ")
+    for i in range(1,11):
+        guess = input("Guess #" + str(i) + ": ")
         if valid_number(guess):
             # compare user's guess with random number and return a clue
+            clue = compare_numbers(guess,number)
+            print(clue)
+            if clue =="You win! Congrats!":
+                
+                break
+            
         else:
             print ("Invalid number.. Try Again")
     
