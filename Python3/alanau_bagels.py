e3d8f2f444319618bca0d05e0f709cef467bedef
Alan Au <alan.au@gmail.com>
2018-04-05 15:30:20 -0700

    Refactoring to be more Alexa-compliant. Might have broken something though.

--- Python3/alanau_bagels.py
+++ Python3/alanau_bagels.py
@@ -25,10 +25,20 @@ def guess_to_list(str_guess):
     return(list_guess)
 
 # test guess vs. secret (both are lists) and return fermis/picos/bagels (str)
-def guess(guess, secret):
+def guess(str_guess, secret):
     fermi = pico = 0
     output = []
-    
+
+    try:
+        int_guess=int(str_guess)
+    except ValueError: # if not an integer, get a new input
+        return("Sorry, I don't know what to do with that input.")
+        
+    if str_guess[0]=='-':
+        return("Sorry, positive numbers only. Please guess again!")
+    elif len(str_guess) != len(secret):
+        return("Oops! Your guess should contain "+str(len(secret))+ " digits. Please try again!")
+
     for index in range(len(guess)):
         digit = guess[index]
         if digit==secret[index]:  # this digit is in the right spot
@@ -44,18 +54,39 @@ def guess(guess, secret):
         output.append("Bagels")
     return(" ".join(output))
 
+# if Bagels, call this to update hint[] to track bad guesses
+def update_hint(list_guess,hint):
+    for digit in list_guess:
+        if len(hint)==0:
+            hint = [int(digit)]
+        elif digit not in hint:
+            hint.append(int(digit))
+        hint.sort()
+    return(hint)
+    
+def give_hint(hint):
+    if len(hint) > 0:
+        print("You know that the answer doesn't contain these numbers:", " ".join(list(map(str,hint))))
+    else:
+        print("Sorry, you haven't learned enough about the numbers in the answer. Try making more guesses!")
+
+def give_answer(secret):
+    return ("The answer is "+"".join(list(map(str,secret))))
+
+def greeting():
+    greet_text = "Welcome to Bagels! Say Help for instructions, or start playing by guessing 3 digits."
+    return(greet_text)
+
+def help():
+    help_text = """Welcome to bagels. I have picked a 3-digit non-repeating number.
+When you guess three digits, I will say Fermi if a correct digit is in the right place.
+I will say Pico is a correcct digit is in teh wrong place.
+I will say Bagels if none of the digits is correct.
+You can also ask for a hint, ask the answer right away, or to stop playing."""
+    return (help_text)
+    
 # Here's a wrapper to run the game:
 def bagels():
-    greeting = "Welcome to Bagels! Type 'help' for instructions."
-    help_text = """The game will generate a 3-digit non-repeating number.
-  'Fermi'  = correct digit in proper place
-  'Pico'   = correct digit in wrong place
-  'Bagels' = no correct digits
-
-Other commands: hint, answer, exit/quit"""
-    
-    print(greeting, flush=True)  # this is *supposed* to print first but doesn't always
-    
     secret = generate_secret()
     done = play_again = False
     hint = []
@@ -65,43 +96,22 @@ Other commands: hint, answer, exit/quit"""
         print(flush=True)
         str_guess = input("Please guess a number: ").lower()
         if str_guess=="answer":
-            print("The answer is:", "".join(list(map(str,secret))))
+            print(give_answer(secret))
             play_again = True
         elif str_guess=="quit" or str_guess=="exit":
             done = True
             continue  # exit immediately
         elif str_guess=="hint":
-            if len(hint) > 0:
-                print("You know it doesn't contain these numbers:", " ".join(list(map(str,hint))))
-            else:
-                print("You don't have any confirmed information yet. Make more guesses!")
+            print(give_hint(hint));
         elif str_guess=="help":
             print(help_text)
         else:
-            try:
-                int_guess=int(str_guess)
-            except ValueError:  # if not an integer, get a new input
-                print("Sorry, I don't know what to do with that input.")
-                continue  # get new input
-                
-            if str_guess[0]=='-':
-                print("Sorry, positive numbers only. Please try again!")
-                continue  # get new input
-            elif len(str_guess) != len(secret):
-                print("Oops! Your guess should contain "+str(len(secret))+ " digits. Please try again!")
-                continue  # get new input
-
-            count += 1  # if it's a valid guess, increment the counter
             list_guess = guess_to_list(str_guess)
             result = guess(list_guess, secret)  # process integer list input
-            if result=="Bagels":  # do some 'hint' processing if we get "Bagels"
-                for digit in list_guess:
-                    if len(hint)==0:
-                        hint = [int(digit)]
-                    elif digit not in hint:
-                        hint.append(int(digit))
-                    hint.sort()
+            hint = update_hint(list_guess, hint)
+            count += 1  # if it's a valid guess, increment the counter #FIX ME
             print(result)
+            
             if result=="Fermi Fermi Fermi":
                 if count==1:
                     print("You got it on your first guess. Lucky!")
